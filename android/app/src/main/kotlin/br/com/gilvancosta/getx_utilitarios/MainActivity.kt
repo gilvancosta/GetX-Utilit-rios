2f2311873d19a6b1382efae62298b9c302b2fe5e
Gilvan Costa <gilvancosta2025@gmail.com>
2023-11-02 19:15:42 -0300

    First Commit

---
+++ android/app/src/main/kotlin/br/com/gilvancosta/getx_utilitarios/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gilvancosta.getx_utilitarios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'pages/home/home_page.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      getPages: [
+        GetPage(
+          name: '/',
+        //  binding: AddressBinding(),
+          page: () => const HomePage(),
+        ),
+/*         GetPage(
+          name: '/getx-controller',
+          binding: BindingsBuilder.put(() => Controller()),
+          page: () => const ControllerPage(),
+        ), */
+      ],
+    );
+  }
+}
